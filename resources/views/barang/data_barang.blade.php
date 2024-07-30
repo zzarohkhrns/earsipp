5c8e1a7273744d72953aba28a3457bf149ed4fcc
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2024-07-30 16:49:09 +0700

    progress

--- resources/views/barang/data_barang.blade.php
+++ resources/views/barang/data_barang.blade.php
@@ -14,10 +14,16 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb m-0 pl-4">
                         <li class="breadcrumb-item active">
-                            <a href="#">ini link</a>
+                            <a href="/{{ $role }}/dashboard">Dashboard</a> / <a>Data barang</a>
                         </li>
                     </ol>
                 </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">{{ Carbon\Carbon::parse(now())->isoFormat('dddd, D MMMM Y') }}
+                        </li>
+                    </ol>
+                </div><!-- /.col -->
             </div>
         </div>
     </div>
@@ -29,51 +35,40 @@
                     <div class="card ijo-atas">
                         <div class="card-body">
                             <div class="row card-data-barang">
-
-
-                            </div>
-                                <div class="tab-content">
-                                    <div class="actvie tab-pane" id="semua">
-
-                                        {{-- isi table --}}
-                                        <section class="content">
-                                            <div class="container-fluid">
-                                                <div class="row card-table-data-barang">
-                                                    <div class="col-12">
-                                                        <table id="example3" class="table table-bordered" style="width:100%">
-                                                            <thead>
-                                                                <tr>
-                                                                    <th>NO</th>
-                                                                    <th>Nama Barang</th>
-                                                                    <th>Satuan</th>
-                                                                    <th>Lokasi Penyimpanan</th>
-                                                                    <th>Spesifikasi</th>
-                                                                    <th>Kontrol Barang</th>
-                                                                    <th>Keluar Masuk</th>
-                                                                    <th>Sisa Barang</th>
-                                                                    <th>Aksi</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td>1</td>
-                                                                    <td>Botol</td>
-                                                                    <td>Pcs</td>
-                                                                    <td>Gudang</td>
-                                                                    <td>Baru</td>
-                                                                    <td>Tanggal</td>
-                                                                    <td>Tanggal</td>
-                                                                    <td>210 pcs</td>
-                                                                    <td>Ubah</td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </section>
-                                    </div>
+                                <div class="col-12">
+                                    <h5 class="d-flex">
+                                        <b class="text-success pl-2">Data Barang<br>Logistik dan Perlengkapan</b>
+                                    </h5>
+                                    <table id="example3" class="table table-bordered" style="width:100%">
+                                        <thead>
+                                            <tr>
+                                                <th>NO</th>
+                                                <th>Nama Barang</th>
+                                                <th>Satuan</th>
+                                                <th>Lokasi Penyimpanan</th>
+                                                <th>Spesifikasi</th>
+                                                <th>Kontrol Barang</th>
+                                                <th>Keluar Masuk</th>
+                                                <th>Sisa Barang</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Botol</td>
+                                                <td>Pcs</td>
+                                                <td>Gudang</td>
+                                                <td>Baru</td>
+                                                <td>Tanggal</td>
+                                                <td>Tanggal</td>
+                                                <td>210 pcs</td>
+                                                <td>Ubah</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
