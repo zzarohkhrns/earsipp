803190eb6f2c85b449018e5922bfcfcc676bffca
R230905 <afrizalfajri23@gmail.com>
2024-11-02 11:29:37 +0700

    feat: create keluar masuk aset migration

---
+++ database/migrations/2024_11_02_111510_create_keluar_masuk_aset_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Expression;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keluar_masuk_aset', function (Blueprint $table) {
+            $gocap = DB::connection('gocap')->getDatabaseName();
+
+            $table->uuid('id_keluar_masuk_aset')->primary();
+            $table->date('tanggal_pencatatan');
+            $table->foreignUuid('id_pencatat')->nullable();
+            $table->foreign('id_pencatat')->references('id_pc_pengurus')->on(new Expression($gocap . '.pc_pengurus'));
+            $table->foreignUuid('id_supervisor')->nullable();
+            $table->foreign('id_supervisor')->references('id_pc_pengurus')->on(new Expression($gocap . '.pc_pengurus'));
+            $table->foreignUuid('id_kc')->nullable();
+            $table->foreign('id_kc')->references('id_pc_pengurus')->on(new Expression($gocap . '.pc_pengurus'));
+            $table->enum('status_pencatatan', ['selesai', 'belum'])->default('belum');
+            $table->enum('status_spv', ['mengetahui', 'belum'])->default('belum');
+            $table->enum('status_kc', ['mengetahui', 'belum'])->default('belum');
+            $table->text('catatan_spv');
+            $table->text('catatan_kc');
+            $table->date('tgl_mengetahui_spv');
+            $table->date('tgl_mengetahui_kc');
+            $table->string('masuk_no_transaksi');
+            $table->date('masuk_tgl_masuk');
+            $table->string('masuk_nama_pemasok');
+            $table->string('masuk_no_faktur');
+            $table->string('masuk_keterangan');
+            $table->string('keluar_no_transaksi');
+            $table->date('keluar_tgl_keluar');
+            $table->string('keluar_nama_pemasok');
+            $table->string('keluar_no_faktur');
+            $table->string('keluar_keterangan');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keluar_masuk_aset');
+    }
+};
