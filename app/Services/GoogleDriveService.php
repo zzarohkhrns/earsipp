353db60e775aa6787ecfa2384f93948f06528aa6
R230905 <afrizalfajri23@gmail.com>
2024-11-07 13:49:45 +0700

    feat: add Google Service
    to store keluar masuk documentation in google drive

---
+++ app/Services/GoogleDriveService.php
@@ -0,0 +1,45 @@
+<?php
+
+// app/Services/GoogleDriveService.php
+namespace App\Services;
+
+use Google\Client;
+use Google\Service\Drive;
+
+class GoogleDriveService
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->client->setAuthConfig(storage_path('app/google/meta-yen-441002-m9-f04ed1a88012.json'));
+        $this->client->addScope(Drive::DRIVE);
+    }
+
+    public function uploadFile($filePath, $fileName)
+    {
+        $driveService = new Drive($this->client);
+
+        $file = new Drive\DriveFile();
+        $file->setName($fileName);
+
+        $result = $driveService->files->create($file, [
+            'data' => file_get_contents($filePath),
+            'mimeType' => mime_content_type($filePath),
+            'uploadType' => 'multipart'
+        ]);
+
+         // Setelah upload berhasil, atur permission untuk file agar dapat diakses publik
+        $permission = new Drive\Permission();
+        $permission->setType('anyone');
+        $permission->setRole('reader');
+        $driveService->permissions->create($result->getId(), $permission);
+
+        // Mengambil link file yang dapat diakses publik
+        $fileId = $result->getId();
+        $driveFileLink = "https://drive.google.com/file/d/$fileId/view";
+
+        return $driveFileLink;
+    }
+}
